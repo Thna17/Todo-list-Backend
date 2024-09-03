dd953ffe5e65d690844cbf08d0d2395d5f91d547
Hong Than brathna <hangbrathna10@gmail.com>
2024-09-03 13:02:03 +0700

    fix

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const cors = require('cors');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
 const { Pool } = require('pg');
 
 const app = express();
